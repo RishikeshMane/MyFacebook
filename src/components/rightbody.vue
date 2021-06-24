1535ed33e03484924bfe699a954d02e5018b1ae2
udaysagar571 <udayrayapalyam@gmail.com>
2021-06-24 13:01:57 +0530

    right body message changed

--- src/components/rightbody.vue
+++ src/components/rightbody.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="right">
-    <span class="Opretprofil">Profile details please</span>
+    <span class="Opretprofil">Profile details changed</span>
     <br />
     <span class="asap">It works quickly.</span>
 
